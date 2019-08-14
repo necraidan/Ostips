c50d20c1fa71d323110da1f05e02ce4cb9ca662e
Benjamin Auzanneau <benjamin.auzanneau@mythalesgroup.io>
2019-08-14 13:39:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ostips
+
+Jm'en calisse
---
+++ scripts/afterDeploy.sh
@@ -0,0 +1,6 @@
+echo "# ostips build [![Build Status](https://travis-ci.org/necraidan/ostips.svg?branch=develop)](https://travis-ci.org/necraidan/ostips)
+---------
+Built from develop branch.
+" >> dist/README.md
+
+cp dist/index.html dist/404.html
